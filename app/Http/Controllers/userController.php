3e87cf421cceaf56720af63713a1b1332ba3cc97
Ayyun Ramzan <ayyun.ramzan@trztechnologies.com>
2022-09-12 12:31:24 +0500

    add this

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class userController extends Controller
+{
+    function index($name){
+      
+        return view('index',['name'=>$name]);
+    }
+}
